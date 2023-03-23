1c6742b638f1d475f5226afc2c174ac9a15db7b5
unknown <asim26k@gmail.com>
2023-03-24 03:19:49 +0530

    implemented routing.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
-  return <div className="App text-red-500">Hello World !!!</div>;
+  return (
+    <div className="App text-red-500">
+      <Routes>
+        <Route to="/" element={<HomePage />} />
+        <Route to="/chats" element={<ChatPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
